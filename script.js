c760c1e463819d91a282cd606778bfbca1a5ac10
Bultel Alexis <bultelalexis@gmail.com>
2020-12-20 20:50:37 +0100

    scroll appear js

--- script.js
+++ script.js
@@ -11,85 +11,84 @@
 //         $('nav ul').toggleClass('active')
 //     })
 // });
+window.onload = function (){
 
+const textAppear = document.querySelectorAll(".hidden");
+const triggerMultiplier = 1.2;
+const windowHeight = window.innerHeight / triggerMultiplier;
 
-window.onload = function () {
-    let navbar = document.querySelectorAll("nav");
-    let black = document.querySelector(".black");
-    var scroll = 0;
+scrollAppear = () => {
+    textAppear.forEach(textAppear => {
 
-    window.addEventListener('scroll', function() {
-        scroll = window.scrollY;
-        navbar.toggle (black);
-    });
-      
+    let positionFromTop = textAppear.getBoundingClientRect().top;
+
+    if (positionFromTop < windowHeight) {
+        textAppear.classList.replace("hidden", "appear");
+    }
+
+  });
+};
+
+window.addEventListener("scroll", scrollAppear);
+
+// navbar
+var navAppear = document.querySelector ("nav");
+var navBlack = document.querySelector (".black");
+
 
 };
-// window.onload = function(){
-// let navbar = document.querySelector("nav");
-// let black = document.querySelector(".black");
-
-// document.querySelector(window).on('scroll', function(){
-//     if (window.scrollTop()) {
-//         document.querySelector('navbar').addClass ('black');
-//     }
-//     else{
-//         document.querySelector('navbar').removeClass ('black');
-//     }
-// });
-// };
 
 /*** Smooth scrolling to page anchor on click**/
-$(function() {
-    $("a[href*='#section1']:not([href='#'])").click(function() {
-        if (
-            location.hostname == this.hostname
-            && this.pathname.replace(/^\//,"") == location.pathname.replace(/^\//,"")
-        ) {
-            var anchor = $(this.hash);
-            anchor = anchor.length ? anchor : $("[name=" + this.hash.slice(1) +"]");
-            if ( anchor.length ) {
-                $("html, body").animate( { scrollTop: anchor.offset().top }, 1500);
-            }
-        }
-    });
+// $(function() {
+//     $("a[href*='#section1']:not([href='#'])").click(function() {
+//         if (
+//             location.hostname == this.hostname
+//             && this.pathname.replace(/^\//,"") == location.pathname.replace(/^\//,"")
+//         ) {
+//             var anchor = $(this.hash);
+//             anchor = anchor.length ? anchor : $("[name=" + this.hash.slice(1) +"]");
+//             if ( anchor.length ) {
+//                 $("html, body").animate( { scrollTop: anchor.offset().top }, 1500);
+//             }
+//         }
+//     });
 
-    $("a[href*='#section2']:not([href='#'])").click(function() {
-        if (
-            location.hostname == this.hostname
-            && this.pathname.replace(/^\//,"") == location.pathname.replace(/^\//,"")
-        ) {
-            var anchor = $(this.hash);
-            anchor = anchor.length ? anchor : $("[name=" + this.hash.slice(1) +"]");
-            if ( anchor.length ) {
-                $("html, body").animate( { scrollTop: anchor.offset().top }, 1500);
-            }
-        }
-    });
+//     $("a[href*='#section2']:not([href='#'])").click(function() {
+//         if (
+//             location.hostname == this.hostname
+//             && this.pathname.replace(/^\//,"") == location.pathname.replace(/^\//,"")
+//         ) {
+//             var anchor = $(this.hash);
+//             anchor = anchor.length ? anchor : $("[name=" + this.hash.slice(1) +"]");
+//             if ( anchor.length ) {
+//                 $("html, body").animate( { scrollTop: anchor.offset().top }, 1500);
+//             }
+//         }
+//     });
 
-    $("a[href*='#section3']:not([href='#'])").click(function() {
-        if (
-            location.hostname == this.hostname
-            && this.pathname.replace(/^\//,"") == location.pathname.replace(/^\//,"")
-        ) {
-            var anchor = $(this.hash);
-            anchor = anchor.length ? anchor : $("[name=" + this.hash.slice(1) +"]");
-            if ( anchor.length ) {
-                $("html, body").animate( { scrollTop: anchor.offset().top }, 1500);
-            }
-        }
-    });
+//     $("a[href*='#section3']:not([href='#'])").click(function() {
+//         if (
+//             location.hostname == this.hostname
+//             && this.pathname.replace(/^\//,"") == location.pathname.replace(/^\//,"")
+//         ) {
+//             var anchor = $(this.hash);
+//             anchor = anchor.length ? anchor : $("[name=" + this.hash.slice(1) +"]");
+//             if ( anchor.length ) {
+//                 $("html, body").animate( { scrollTop: anchor.offset().top }, 1500);
+//             }
+//         }
+//     });
 
-    $("a[href*='#section4']:not([href='#'])").click(function() {
-        if (
-            location.hostname == this.hostname
-            && this.pathname.replace(/^\//,"") == location.pathname.replace(/^\//,"")
-        ) {
-            var anchor = $(this.hash);
-            anchor = anchor.length ? anchor : $("[name=" + this.hash.slice(1) +"]");
-            if ( anchor.length ) {
-                $("html, body").animate( { scrollTop: anchor.offset().top }, 1500);
-            }
-        }
-    });
-});
+//     $("a[href*='#section4']:not([href='#'])").click(function() {
+//         if (
+//             location.hostname == this.hostname
+//             && this.pathname.replace(/^\//,"") == location.pathname.replace(/^\//,"")
+//         ) {
+//             var anchor = $(this.hash);
+//             anchor = anchor.length ? anchor : $("[name=" + this.hash.slice(1) +"]");
+//             if ( anchor.length ) {
+//                 $("html, body").animate( { scrollTop: anchor.offset().top }, 1500);
+//             }
+//         }
+//     });
+// });
