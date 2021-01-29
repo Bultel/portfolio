b414783392fd71dd37b43d24eadd7a5f8b0fbf39
Bultel Alexis <bultelalexis@gmail.com>
2021-01-29 15:41:57 +0100

    final

--- script.js
+++ script.js
@@ -1,68 +1,68 @@
-window.onload = function (){
+window.onload = function () {
 
-// Scroll appear
+  // Scroll appear
 
-const textAppear = document.querySelectorAll(".hidden");
-const triggerMultiplier = 1.2;
-const windowHeight = window.innerHeight / triggerMultiplier;
+  const textAppear = document.querySelectorAll(".hidden");
+  const triggerMultiplier = 1.2;
+  const windowHeight = window.innerHeight / triggerMultiplier;
 
-scrollAppear = () => {
+  scrollAppear = () => {
     textAppear.forEach(textAppear => {
 
-    let positionFromTop = textAppear.getBoundingClientRect().top;
+      let positionFromTop = textAppear.getBoundingClientRect().top;
 
-    if (positionFromTop < windowHeight) {
+      if (positionFromTop < windowHeight) {
         textAppear.classList.replace("hidden", "appear");
-    }
+      }
 
-  });
-};
-
-window.addEventListener("scroll", scrollAppear);
-
-// navbar
-
-const navbarToggler = document.querySelector(".navbar-toggler");
-const navbarMenu = document.querySelector(".navbar ul");
-const navbarLinks = document.querySelectorAll(".navbar a");
-
-navbarToggler.addEventListener("click", navbarTogglerClick);
-
-function navbarTogglerClick() {
-  navbarToggler.classList.toggle("open-navbar-toggler");
-  navbarMenu.classList.toggle("open");
-}
-
-navbarLinks.forEach(elem => elem.addEventListener("click", navbarLinkClick));
-
-function navbarLinkClick() {
-  if(navbarMenu.classList.contains("open")) {
-    navbarToggler.click();
-  }
-}
-
-// Typin effect
-
-const texts = ["websites", "illustrations", "pancakes"];
-let count = 0; //Words
-let index = 0; //individuals letters
-let currentText = '';
-let letter = '';
-
-(function type() {
-  if (count === texts.length){
-    count = 0;
+    });
   };
-  currentText = texts [count];
-  letter = currentText.slice (0, ++index);
 
-  document.querySelector (".typing").textContent = letter;
-  if (letter.length === currentText.length) {
-    count++;
-    index = 0;
+  window.addEventListener("scroll", scrollAppear);
+
+  // navbar
+
+  const navbarToggler = document.querySelector(".navbar-toggler");
+  const navbarMenu = document.querySelector(".navbar ul");
+  const navbarLinks = document.querySelectorAll(".navbar a");
+
+  navbarToggler.addEventListener("click", navbarTogglerClick);
+
+  function navbarTogglerClick() {
+    navbarToggler.classList.toggle("open-navbar-toggler");
+    navbarMenu.classList.toggle("open");
   }
-  setTimeout (type,400);
 
-}());
-  
+  navbarLinks.forEach(elem => elem.addEventListener("click", navbarLinkClick));
+
+  function navbarLinkClick() {
+    if (navbarMenu.classList.contains("open")) {
+      navbarToggler.click();
+    }
+  }
+
+  // Typin effect
+
+  const texts = ["websites", "illustrations", "pancakes"];
+  let count = 0; //Words
+  let index = 0; //individuals letters
+  let currentText = '';
+  let letter = '';
+
+  (function type() {
+    if (count === texts.length) {
+      count = 0;
+    };
+    currentText = texts[count];
+    letter = currentText.slice(0, ++index);
+
+    document.querySelector(".typing").textContent = letter;
+    if (letter.length === currentText.length) {
+      count++;
+      index = 0;
+    }
+    setTimeout(type, 400);
+
+  }());
+
 };
